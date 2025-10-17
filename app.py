b6de99128bf606305e039bc84e43705f46351462
Amosov Vyacheslav <amosov_v_v@mail.ru>
2025-10-17 15:58:14 +0300

    Уточнение главного вопроса

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/bin/python3
 
-ultimate_question = 'Главный вопрос Жизни,Вселенной и Вообще!!!'
+ultimate_question = 'Главный вопрос Жизни, Вселенной и тд, и тп'
 
 print(f'The ulimate answer is {len(ultimate_question)}')
